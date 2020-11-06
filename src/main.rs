fab108d8e0bf20df118290ab9e08ed2a21282203
Jonas <jonas@jmp.blue>
2020-11-06 10:57:45 +0100

    print to serial

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -22,27 +23,34 @@ pub extern "C" fn _start() -> ! {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    let prev_color = vga_buffer::WRITER.lock().get_color();
-    vga_buffer::WRITER.lock().set_color(vga_buffer::Color::Yellow, vga_buffer::Color::Black);
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
-    vga_buffer::WRITER.lock().set_color_code(prev_color);
     exit_qemu(QemuExitCode::Success)
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     vga_buffer::WRITER.lock().set_color(vga_buffer::Color::Red, vga_buffer::Color::Black);
     println!("\n{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
